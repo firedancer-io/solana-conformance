1ccfdad59fdd8cbac15e08b2a9a0a20ea087708f
Richard Patel <ripatel@jumptrading.com>
2024-04-01 18:01:52 +0000

    Fix Base58 decode

--- superbased58.pyx
+++ superbased58.pyx
@@ -2,7 +2,7 @@ cimport superbased58
 
 # Constants for buffer sizes including null terminator
 cpdef encode_32(bytes data):
-    cdef char out_buffer[45]
+    cdef char[45] out_buffer
     cdef unsigned long length = 0
     cdef char* result = superbased58.fd_base58_encode_32(<unsigned char*> data, &length, out_buffer)
     if result == NULL:
@@ -10,7 +10,7 @@ cpdef encode_32(bytes data):
     return out_buffer[:length]
 
 cpdef encode_64(bytes data):
-    cdef char out_buffer[89]
+    cdef char[89] out_buffer
     cdef unsigned long length = 0
     cdef char* result = superbased58.fd_base58_encode_64(<unsigned char*> data, &length, out_buffer)
     if result == NULL:
@@ -18,15 +18,15 @@ cpdef encode_64(bytes data):
     return out_buffer[:length]
 
 cpdef decode_32(bytes encoded):
-    cdef unsigned char out_buffer[32]
+    cdef unsigned char[32] out_buffer
     cdef unsigned char* result = superbased58.fd_base58_decode_32(<const char*>encoded, out_buffer)
     if result == NULL:
         raise ValueError("Failed to decode data.")
-    return bytes(out_buffer)
+    return out_buffer[:32]
 
 cpdef decode_64(bytes encoded):
-    cdef unsigned char out_buffer[64]
+    cdef unsigned char[64] out_buffer
     cdef unsigned char* result = superbased58.fd_base58_decode_64(<const char*>encoded, out_buffer)
     if result == NULL:
         raise ValueError("Failed to decode data.")
-    return bytes(out_buffer)
+    return out_buffer[:64]
