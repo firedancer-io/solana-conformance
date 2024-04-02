bf34dac90bf8bc7001e96c69dc523760cc6abe74
Manik Jain <mjain@jumptrading.com>
2024-04-02 17:22:44 +0000

    Update README + docs

--- src/test_suite/test_suite.py
+++ src/test_suite/test_suite.py
@@ -9,7 +9,14 @@ from google.protobuf import text_format
 from test_suite.constants import LOG_FILE_SEPARATOR_LENGTH
 import test_suite.invoke_pb2 as pb
 from test_suite.codec_utils import encode_input
-from test_suite.multiprocessing_utils import check_consistency_in_results, generate_test_case, initialize_process_output_buffers, merge_results_over_iterations, process_single_test_case, build_test_results
+from test_suite.multiprocessing_utils import (
+    check_consistency_in_results,
+    generate_test_case,
+    initialize_process_output_buffers,
+    merge_results_over_iterations,
+    process_single_test_case,
+    build_test_results,
+)
 import test_suite.globals as globals
 from test_suite.debugger import debug_host
 import resource
@@ -17,37 +24,32 @@ import subprocess
 import tempfile
 
 
-app = typer.Typer(help="Validate instruction effects from clients using instruction context Protobuf messages.")
+app = typer.Typer(
+    help="Validate instruction effects from clients using instruction context Protobuf messages."
+)
 
 
 @app.command()
 def debug_instruction(
-    file: Path = typer.Option(
-        None,
-        "--input",
-        "-i",
-        help="Input file"
-    ),
+    file: Path = typer.Option(None, "--input", "-i", help="Input file"),
     shared_library: Path = typer.Option(
         Path("impl/firedancer/build/native/clang/lib/libfd_exec_sol_compat.so"),
-        "--executable-path",
-        "-e",
-        help="Path to the binary executable to debug"
+        "--target",
+        "-t",
+        help="Shared object (.so) target file path to debug",
     ),
     debugger: str = typer.Option(
-        "gdb",
-        "--debugger",
-        "-d",
-        help="Debugger to use (gdb, rust-gdb)"
-    )
+        "gdb", "--debugger", "-d", help="Debugger to use (gdb, rust-gdb)"
+    ),
 ):
     # Decode the file and reencode as binary (since it may be in text format)
-    print("-"*LOG_FILE_SEPARATOR_LENGTH)
+    print("-" * LOG_FILE_SEPARATOR_LENGTH)
     print(f"Processing {file.name}...")
     _, instruction_context = generate_test_case(file)
     assert instruction_context is not None, f"Unable to read {file.name}"
 
     debug_host(shared_library, instruction_context, gdb=debugger)
+
 
 @app.command()
 def consolidate_logs(
@@ -55,20 +57,17 @@ def consolidate_logs(
         Path("test_results"),
         "--input-dir",
         "-i",
-        help="Input directory containing test results"
+        help="Input directory containing test results",
     ),
     output_dir: Path = typer.Option(
         Path("consolidated_logs"),
         "--output-dir",
         "-o",
-        help="Output directory for consolidated logs"
+        help="Output directory for consolidated logs",
     ),
     chunk_size: int = typer.Option(
-        10000,
-        "--chunk-size",
-        "-c",
-        help="Number of test results per file"
-    )
+        10000, "--chunk-size", "-c", help="Number of test results per file"
+    ),
 ):
     # Create the output directory, if necessary
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -87,15 +86,19 @@ def consolidate_logs(
         for i in range(len(log_files)):
             # Open a new log file every chunk_size test cases
             if i % chunk_size == 0:
-                if current_log_file: current_log_file.close()
-                current_log_file = open(output_dir / lib / f"{log_files[i].stem}.txt", "w")
+                if current_log_file:
+                    current_log_file.close()
+                current_log_file = open(
+                    output_dir / lib / f"{log_files[i].stem}.txt", "w"
+                )
 
             # Write test case name + log contents + separators
             current_log_file.write(log_files[i].stem + ":\n")
             current_log_file.write(log_files[i].read_text())
-            current_log_file.write("\n" + "-"*LOG_FILE_SEPARATOR_LENGTH + "\n")
+            current_log_file.write("\n" + "-" * LOG_FILE_SEPARATOR_LENGTH + "\n")
 
-        if current_log_file: current_log_file.close()
+        if current_log_file:
+            current_log_file.close()
 
 
 @app.command()
@@ -104,38 +107,32 @@ def check_consistency(
         Path("corpus8"),
         "--input-dir",
         "-i",
-        help="Input directory containing instruction context messages"
+        help="Input directory containing instruction context messages",
     ),
     shared_libraries: List[Path] = typer.Option(
-        [],
-        "--target",
-        "-t",
-        help="Shared object (.so) target file paths"
+        [], "--target", "-t", help="Shared object (.so) target file paths"
     ),
     output_dir: Path = typer.Option(
         Path("consistency_results"),
         "--output-dir",
         "-o",
-        help="Output directory for test results"
+        help="Output directory for test results",
     ),
     num_iterations: int = typer.Option(
         2,
         "--num-iterations",
         "-n",
-        help="Number of consistency iterations to run for each library"
+        help="Number of consistency iterations to run for each library",
     ),
     num_processes: int = typer.Option(
-        4,
-        "--num-processes",
-        "-p",
-        help="Number of processes to use"
+        4, "--num-processes", "-p", help="Number of processes to use"
     ),
     randomize_output_buffer: bool = typer.Option(
         False,
         "--randomize-output-buffer",
         "-r",
-        help="Randomizes bytes in output buffer before shared library execution"
-    )
+        help="Randomizes bytes in output buffer before shared library execution",
+    ),
 ):
     # Initialize globals
     globals.output_dir = output_dir
@@ -162,12 +159,20 @@ def check_consistency(
             # Initialize the libraries for each iteration
             for iteration in range(globals.n_iterations):
                 # Make output directory
-                (globals.output_dir / target.stem / str(iteration)).mkdir(parents=True, exist_ok=True)
+                (globals.output_dir / target.stem / str(iteration)).mkdir(
+                    parents=True, exist_ok=True
+                )
 
         # Process the test cases in parallel through shared libraries for n interations
         print("Executing tests...")
-        with Pool(processes=num_processes, initializer=initialize_process_output_buffers, initargs=(randomize_output_buffer,)) as pool:
-            execution_results = pool.starmap(process_single_test_case, execution_contexts)
+        with Pool(
+            processes=num_processes,
+            initializer=initialize_process_output_buffers,
+            initargs=(randomize_output_buffer,),
+        ) as pool:
+            execution_results = pool.starmap(
+                process_single_test_case, execution_contexts
+            )
             results_per_iteration.append(execution_results)
 
         print("Cleaning up...")
@@ -176,21 +181,21 @@ def check_consistency(
 
     # Build the results properly
     with Pool(processes=num_processes) as pool:
-        execution_results = pool.map(merge_results_over_iterations, zip(*results_per_iteration))
+        execution_results = pool.map(
+            merge_results_over_iterations, zip(*results_per_iteration)
+        )
 
     # Process the test results in parallel
     print("Building test results...")
     with Pool(processes=num_processes) as pool:
-        test_case_results = pool.starmap(check_consistency_in_results, execution_results)
+        test_case_results = pool.starmap(
+            check_consistency_in_results, execution_results
+        )
 
     # Compute per-library results
     library_results = {}
     for library in globals.target_libraries:
-        library_results[library] = {
-            "passed": 0,
-            "failed": 0,
-            "skipped": 0
-        }
+        library_results[library] = {"passed": 0, "failed": 0, "skipped": 0}
 
     # Build the results
     for result in test_case_results:
@@ -202,14 +207,16 @@ def check_consistency(
     peak_memory_usage_kb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
     print(f"Peak Memory Usage: {peak_memory_usage_kb / 1024} MB")
 
-    print("-"*LOG_FILE_SEPARATOR_LENGTH)
+    print("-" * LOG_FILE_SEPARATOR_LENGTH)
 
     for library in globals.target_libraries:
         results = library_results[library]
         print(f"{library} results")
         print(f"Total test cases: {sum(results.values())}")
-        print(f"Passed: {results['passed']}, Failed: {results['failed']}, Skipped: {results['skipped']}")
-        print("-"*LOG_FILE_SEPARATOR_LENGTH)
+        print(
+            f"Passed: {results['passed']}, Failed: {results['failed']}, Skipped: {results['skipped']}"
+        )
+        print("-" * LOG_FILE_SEPARATOR_LENGTH)
 
 
 @app.command()
@@ -218,38 +225,32 @@ def run_tests(
         Path("corpus8"),
         "--input-dir",
         "-i",
-        help="Input directory containing instruction context messages"
+        help="Input directory containing instruction context messages",
     ),
     solana_shared_library: Path = typer.Option(
         Path("impl/lib/libsolfuzz_agave_v2.0.so"),
         "--solana-target",
         "-s",
-        help="Solana (or ground truth) shared object (.so) target file path"
+        help="Solana (or ground truth) shared object (.so) target file path",
     ),
     shared_libraries: List[Path] = typer.Option(
-        [],
-        "--target",
-        "-t",
-        help="Shared object (.so) target file paths"
+        [], "--target", "-t", help="Shared object (.so) target file paths"
     ),
     output_dir: Path = typer.Option(
         Path("test_results"),
         "--output-dir",
         "-o",
-        help="Output directory for test results"
+        help="Output directory for test results",
     ),
     num_processes: int = typer.Option(
-        4,
-        "--num-processes",
-        "-p",
-        help="Number of processes to use"
+        4, "--num-processes", "-p", help="Number of processes to use"
     ),
     randomize_output_buffer: bool = typer.Option(
         False,
         "--randomize-output-buffer",
         "-r",
-        help="Randomizes bytes in output buffer before shared library execution"
-    )
+        help="Randomizes bytes in output buffer before shared library execution",
+    ),
 ):
     # Add Solana library to shared libraries
     shared_libraries = [solana_shared_library] + shared_libraries
@@ -279,9 +280,11 @@ def run_tests(
 
     # Process the test cases in parallel through shared libraries
     print("Executing tests...")
-    with Pool(processes=num_processes,
-              initializer=initialize_process_output_buffers,
-              initargs=(randomize_output_buffer,)) as pool:
+    with Pool(
+        processes=num_processes,
+        initializer=initialize_process_output_buffers,
+        initargs=(randomize_output_buffer,),
+    ) as pool:
         execution_results = pool.starmap(process_single_test_case, execution_contexts)
 
     # Process the test results in parallel
@@ -310,14 +313,14 @@ def decode_protobuf(
         Path("raw_instruction_context"),
         "--input-dir",
         "-i",
-        help="Input directory containing instruction context messages in binary format"
+        help="Input directory containing instruction context messages in binary format",
     ),
     output_dir: Path = typer.Option(
         Path("readable_instruction_context"),
         "--output-dir",
         "-o",
-        help="Output directory for base58-encoded, human-readable instruction context messages"
-    )
+        help="Output directory for base58-encoded, human-readable instruction context messages",
+    ),
 ):
     # Create the output directory, if necessary
     output_dir.mkdir(parents=True, exist_ok=True)
