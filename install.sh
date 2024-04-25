44314da180320b0f718171811805934a13bf52bb
Manik Jain <mjain@jumptrading.com>
2024-04-25 17:22:49 +0000

    Fix Python.h dep issue

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo dnf install -y gcc-toolset-12 || true
 source /opt/rh/gcc-toolset-12/enable
 python3.11 -m venv test_suite_env
 source test_suite_env/bin/activate
+sudo dnf install -y python3.11-devel || true
 make -j -C impl
 pip install .
 protoc --python_out=src/test_suite invoke.proto
