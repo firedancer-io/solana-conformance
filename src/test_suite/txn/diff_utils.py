b43619e243499013114245a018d314b3cf20bc9f
Manik Jain <mjain@jumptrading.com>
2024-07-23 22:11:40 +0000

    Comments

--- src/test_suite/txn/diff_utils.py
+++ src/test_suite/txn/diff_utils.py
@@ -2,12 +2,14 @@ import test_suite.txn_pb2 as txn_pb
 
 
 def txn_diff_effects(a_san: txn_pb.TxnResult, b_san: txn_pb.TxnResult):
+    # Don't compare rent epochs
     for i in range(len(a_san.resulting_state.acct_states)):
         a_san.resulting_state.acct_states[i].rent_epoch = 0
 
     for i in range(len(b_san.resulting_state.acct_states)):
         b_san.resulting_state.acct_states[i].rent_epoch = 0
 
+    # Don't compare transaction statuses and compute units if both txns fail
     if a_san.status and b_san.status:
         a_san.status = 1
         b_san.status = 1
