a49f7fb007a55c4cb4526e2118368558af2ee95a
Kunal Bhargava <kbhargava@jumptrading.com>
2025-09-17 16:28:27 +0000

    slot number examples

--- src/test_suite/instr/transform_utils.py
+++ src/test_suite/instr/transform_utils.py
@@ -3,4 +3,28 @@ import test_suite.invoke_pb2 as invoke_pb
 
 
 def transform_fixture(fixture: invoke_pb.InstrFixture):
+    """
+    Example:
+
+    accounts = fixture.input.accounts
+
+    for i in range(len(accounts)):
+        if accounts[i].address == fd58.dec32(b"SysvarC1ock11111111111111111111111111111111"):
+            data_array = bytearray(accounts[i].data)
+            if (len(data_array) < 8):
+                return
+            data_array[4] = data_array[4] & 0x00
+            data_array[5] = data_array[5] & 0x00
+            data_array[6] = data_array[6] & 0x00
+            data_array[7] = data_array[7] & 0x00
+            accounts[i].data = bytes(data_array)
+
+            slot_array = bytearray(8)
+            slot_array[3] = data_array[3]
+            slot_array[2] = data_array[2]
+            slot_array[1] = data_array[1]
+            slot_array[0] = data_array[0]
+            fixture.input.slot_context.slot = int.from_bytes(slot_array, "little")
+    """
+
     pass
--- src/test_suite/syscall/transform_utils.py
+++ src/test_suite/syscall/transform_utils.py
@@ -28,5 +28,28 @@ def transform_fixture(fixture: vm_pb.SyscallFixture):
             return
 
     fixture.input.instr_ctx.accounts.extend([program_account])
+
+    Example:
+
+    accounts = fixture.input.instr_ctx.accounts
+
+    for i in range(len(accounts)):
+        if accounts[i].address == fd58.dec32(b"SysvarC1ock11111111111111111111111111111111"):
+            data_array = bytearray(accounts[i].data)
+            if (len(data_array) < 8):
+                return
+            data_array[4] = data_array[4] & 0x00
+            data_array[5] = data_array[5] & 0x00
+            data_array[6] = data_array[6] & 0x00
+            data_array[7] = data_array[7] & 0x00
+            accounts[i].data = bytes(data_array)
+
+            slot_array = bytearray(8)
+            slot_array[3] = data_array[3]
+            slot_array[2] = data_array[2]
+            slot_array[1] = data_array[1]
+            slot_array[0] = data_array[0]
+            fixture.input.instr_ctx.slot_context.slot = int.from_bytes(slot_array, "little")
     """
+
     pass
